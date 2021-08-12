7721fe85b2a302286cd11c47af1b9bd5accbf937
matthew-mcgrath <62624146+matthew-mcgrath@users.noreply.github.com>
2021-08-12 15:22:54 -0500

    Update baby_v2.py
    Upgrading to f strings and changing to single quotes.

--- baby_v2.py
+++ baby_v2.py
@@ -7,10 +7,10 @@ baby = life.create()
 
 due_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1639288801))
 
-parents = ["My wife", "I"]
+parents = ['My wife', 'I']
 
 try:
     for b in baby:
-        print("Baby McGrath #2 is due on " + due_date + "!  " + parents[0] + " and " + parents[1] + " are quite excited.  Cheers!")
+        print(f'Baby McGrath version 2 is due on {due_date}!  {parents[0]} and {parents[1]} are quite excited.  Cheers!')
 except Exception as e:
     print(e)        
